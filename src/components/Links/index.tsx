386ac2ea76f3ef1701c0dd8c13a60505a6b01393
Edgar Burtnieks <edburtnieks@gmail.com>
2020-05-05 23:55:20 +0300

    Add link to process post in footer

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { ReactComponent as GitHubIcon } from "../../assets/icons/github.svg";
 import { ReactComponent as BriefcaseIcon } from "../../assets/icons/briefcase.svg";
+import { ReactComponent as RefreshCWIcon } from "../../assets/icons/refresh-cw.svg";
 
 import styles from "./styles.module.scss";
 
@@ -29,6 +30,18 @@ const Links = () => {
         >
           <BriefcaseIcon className={styles.icon} />
           <span>Portfolio</span>
+        </a>
+      </li>
+
+      <li className={styles.listItem}>
+        <a
+          href="https://www.edburtnieks.me/examples/bowling-reservation-form/"
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <RefreshCWIcon className={styles.icon} />
+          <span>Process</span>
         </a>
       </li>
     </ul>
